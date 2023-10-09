78958d494dc06968124110dc789e213a928a893e
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-09 15:10:34 -0400

    Feat: send variables

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,13 @@
+const object = {
+  title: "React Vite App",
+  subtitle: "Subtitle",
+};
+
 export const App = () => {
   return (
     <>
-      <h1>React Vite App</h1>
+      <h1>{object.title}</h1>
+      <pre>{JSON.stringify(object)}</pre>
     </>
-  )
-}
+  );
+};
