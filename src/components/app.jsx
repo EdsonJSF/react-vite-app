06cffaca9d2a496f506940a7afb1d6954e0ab3ff
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-10 21:30:49 -0400

    Feat: Props types

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
+import PropTypes from "prop-types";
+
 const object = {
   title: "React Vite App",
   subtitle: "Subtitle",
 };
 
-export const App = ({number}) => {
+export const App = ({ number }) => {
   return (
     <>
       <h1>{object.title}</h1>
@@ -11,4 +13,8 @@ export const App = ({number}) => {
       <p>{number + number}</p>
     </>
   );
+};
+
+App.prototype = {
+  number: PropTypes.number,
 };
