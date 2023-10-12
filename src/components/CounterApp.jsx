5d05bb78500bdcd8299029dfb038cd51e78e06b2
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-11 21:13:55 -0400

    Feat: Event onClick

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 
+const incrementValue = (event) => {
+  console.log(event);
+};
+
 export const CounterApp = ({ value }) => {
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{value}</h2>
+
+      <button onClick={incrementValue}>+1</button>
     </>
   );
 };
