67cf435003b2ee7034c0b5e15bd174658799832d
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-11 21:27:17 -0400

    Feat: useState

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
-const incrementValue = (event) => {
-  console.log(event);
-};
-
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const incrementValue = () => {
+    setCounter(counter + 1);
+    // setCounter((c) => c + 1);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={incrementValue}>+1</button>
     </>
