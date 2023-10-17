90ce5f87a8db4a2ed877b3db594633ce3f91e911
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-16 21:35:51 -0400

    Feat: Jest - Expect - toBe

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,5 +1,12 @@
-test("Is a test without fails", () => {
-  if (1 === 0) {
-    throw new Error("An error");
-  }
+describe("Test on <DemoComponent />", () => {
+  test("Is a test without fails", () => {
+    // 1. Insert
+    const message1 = "Test";
+
+    // 2. Stimulation
+    const message2 = message1.trim();
+
+    // 3. Check status
+    expect(message1).toBe(message2);
+  });
 });
