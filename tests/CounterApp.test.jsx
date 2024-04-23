914d82e08a24b7630ab335b73bf9563be38b76d6
EdsonJSF <edsonjsf1990@gmail.com>
2024-04-23 12:04:20 -0400

    feat: testing using events which are simulated

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/components/CounterApp";
 
 describe("Test on <CounterApp />", () => {
@@ -17,5 +17,27 @@ describe("Test on <CounterApp />", () => {
     expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain(
       value.toString()
     );
+  });
+
+  test("should increment with +1 btn", () => {
+    render(<CounterApp value={value} />);
+
+    fireEvent.click(screen.getByText("+1"));
+    expect(screen.getByText(value + 1)).toBeTruthy();
+  });
+
+  test("should decrement with -1 btn", () => {
+    render(<CounterApp value={value} />);
+
+    fireEvent.click(screen.getByText("-1"));
+    expect(screen.getByText(value - 1)).toBeTruthy();
+  });
+
+  test("should work reset btn", () => {
+    render(<CounterApp value={value} />);
+
+    fireEvent.click(screen.getByText("+1"));
+    fireEvent.click(screen.getByText("Reset"));
+    expect(screen.getByText(value)).toBeTruthy();
   });
 });
